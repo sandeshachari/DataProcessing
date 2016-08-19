611708f5a78977d842cdca2871606aba774b41b7
Sandesh <sandesh.achari@sedemac.com>
2016-08-19 15:06:16 +0530

    Rpm is plotted.

--- veh_map.py
+++ veh_map.py
@@ -41,12 +41,48 @@ print("size of cps = ",len(cps))
 print("size of ign = ",len(ign))
 print("size of time = ",len(time))
 
-plt.figure()
-plt.plot(time, cps, time, ign)
-plt.xlabel('time')
-plt.ylabel('voltage')
-plt.grid()
-plt.show()
 
-# pylab.savefig('n21_data_3.png')
+
+cpsAdvLevel = 4
+cpsZeroLevel = -4
+cpsAdv=0
+timeAdvDetected = 0
+rpmFromAdv = np.zeros(len(cps))
+for i in range(len(cps)):
+	if i > 0:
+		rpmFromAdv[i] = rpmFromAdv[i - 1]
+	if(cps[i] > cpsAdvLevel):
+		cpsAdv = cpsAdv + 1;
+		if((cpsAdvLevelDetected == False)):
+			if (cpsAdv > 1):
+				diffTimeAdvDetected = time[i] - timeAdvDetected
+				rpmFromAdv[i] = 60/diffTimeAdvDetected
+			timeAdvDetected = time[i]
+		cpsAdvLevelDetected = True
+	else:
+		cpsAdvLevelDetected = False
+	if (cps[i] < cpsZeroLevel):
+		cpsZeroLevelDetected = True
+	else:
+		cpsZeroLevelDetected = False
+
+if 1:
+	if 0:		# decide whether to plot only cps and ign or rpm also
+		plt.figure()
+		plt.plot(time, cps, time, ign)
+		plt.xlabel('time')
+		plt.ylabel('voltage')
+	else:
+		fig, ax1 = plt.subplots()
+		ax2 = ax1.twinx()
+		ax1.plot(time, cps, time, ign)
+		ax2.plot(time,rpmFromAdv, '-r')
+		# plt.xlabel('time')
+		# plt.ylabel('voltage')		
+	plt.grid()	
+	if 1:		# decide whether to show the plot or save the file. 
+		plt.show()
+	else:		
+		# pylab.savefig('n21_data_3.png')
+		pass
 
