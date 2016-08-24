28ba9f89f41d4c9d9437d224ee428c9f4ab58104
Sandesh <sandesh.achari@sedemac.com>
2016-08-24 14:31:45 +0530

    Working fine: single_pip_tci, single_pip_cdi, dual_pip_cdi. Automatic cps zero pulse and cdi ign detection level set. Verified with available data.

--- veh_map.py
+++ veh_map.py
@@ -8,7 +8,7 @@ cases:
 2. 	Dual pip, Ign in each cycle
 3. 	Multi pip, Ign in each thermo-cycle, fi in each thermo-cycle  <-- FI
 """
-#E:\python_scripts\Veh_Mapping\dual_pip\dual_pip_data.mat
+
 #!/usr/bin/python
 
 import scipy.io as sio
@@ -16,13 +16,16 @@ import pylab
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import math
 
 
 if 1:
-	cpsType = raw_input('Enter the type of cps and ign (single_pip_tci/dual_pip_tci/single_pip_cdi/dual_pip_cdi): ')
+	cpsType = raw_input('\nEnter the type of cps and ign (single_pip_tci/dual_pip_tci/single_pip_cdi/dual_pip_cdi): ')
 	if cpsType == 'single_pip_tci' or cpsType == 'dual_pip_tci' or cpsType == 'single_pip_cdi' or cpsType == 'dual_pip_cdi':	# or cpsType == 'multi_pip':
 		print '\nCps type is: ', cpsType ,'\n'
-		print 'Note: Pls make sure that pico data should be filtered with at least 10kHz. \n      and it is stored in the .mat file.\n'
+		print 'Note: Pls make sure that \n1. Pico data is filtered with at least 10kHz. \n2. It is stored in the .mat file.' \
+		'\n3. Ign/Inj angle is calculated from the minimum of zero pulse.\n'
+
 		# sys.exit(1)
 	else:
 		raise ValueError('Pls enter the valid input')
@@ -78,6 +81,7 @@ cpsZeroLevel = -4
 
 dwellStartLevel = 4
 dwellEndLevel = 4
+cdiIgnDetectionLevel = -6
 
 # Configurable parameters: end
 
@@ -94,9 +98,11 @@ ignAngleList = []
 timeDwellOnList = []
 
 cpsZeroLevelDetected = False
+cpsAdvLevelDetected = False
 
 diffTimeZeroDetected = 100000
 diffTimeZeroDetectedOld = 100000
+diffTimeZerotoZeroDetected = 100000
 
 timeDwellStartDetected = 0
 timeDwellEndDetected = 0
@@ -110,12 +116,13 @@ cdiIgnDetected = False
 timeDwellStartDetected = 0
 cpsAdvForIgnDetected = False
 diffTimeZeroToZeroDetected = 0
-cdiIgnDetectionLevel = -6	
 timeCdiIgnDetected = 0
 itSparked = False
 avgDwellOnTime = 0
 
 rpmFromZeroList = []
+entered = 0
+cpsZeroBig = 0
 
 for i in range(len(cps)):
 
@@ -148,30 +155,54 @@ for i in range(len(cps)):
 
 
 	if (cps[i] < cpsZeroLevel):
-		cpsZero = cpsZero + 1;
 		if((cpsZeroLevelDetected == False)):
+			cpsZero = cpsZero + 1;
 			if cpsType == 'single_pip_tci' or cpsType == 'single_pip_cdi':
 				if (cpsZero > 1):
 					diffTimeZeroDetected = time[i] - timeZeroDetected
 					rpmFromZero[i] = 60/diffTimeZeroDetected			
 					rpmFromZeroList.append(round(rpmFromZero[i],2))
-
+					if cpsZero == 3:
+						cpsZeroLevel = -(int)((3 + 2*((float)(rpmFromZero[i]) - 300)/2700)*(rpmFromZero[i] < 3000) + 4*(rpmFromZero[i] >= 3000))
+						print '\ncpsZeroLevel = ', cpsZeroLevel						
+						if cpsType == 'single_pip_tci':
+							dwellStartLevel = 4
+							dwellEndLevel = 4
+						else:
+							cdiIgnDetectionLevel = -(int)((2 + 4*((float)(rpmFromZero[i]) - 300)/2700)*(rpmFromZero[i] < 3000)	\
+							+ 6*(rpmFromZero[i] >= 3000))	
+							print '\ncdiIgnDetectionLevel = ',cdiIgnDetectionLevel
+						entered = 34
 				timeZeroDetected = time[i]		
 			else:
 				diffTimeZeroDetected = time[i] - timeZeroDetected
 				if(cpsAdv > 2):
 					if int(diffTimeZeroDetected/diffTimeZeroDetectedOld) == 0:
 						# Small pip is ignored
+						cpsZeroBig = cpsZeroBig + 1
 						rpmFromZero[i] = 60/(diffTimeZeroDetected + diffTimeZeroDetectedOld)
 						rpmFromZeroList.append(round(rpmFromZero[i],2))
 						diffTimeZeroToZeroDetected = diffTimeZeroDetected + diffTimeZeroDetectedOld
+						if cpsZeroBig == 2:
+							cpsZeroLevel = -(int)((3 + 2*((float)(rpmFromZero[i]) - 300)/2700)*(rpmFromZero[i] < 3000) + 4*(rpmFromZero[i] >= 3000))
+							if cpsType == 'dual_pip_tci':
+								dwellStartLevel = 4
+								dwellEndLevel = 4
+							else:
+								cdiIgnDetectionLevel = -(int)((3 + 4*((float)(rpmFromZero[i]) - 300)/2700)*(rpmFromZero[i] < 3000)	\
+								+ 6*(rpmFromZero[i] >= 3000))	
+								# cdiIgnDetectionLevel = -6		
+							print '\ncpsZeroLevel = ', cpsZeroLevel
+							print '\ncdiIgnDetectionLevel = ',cdiIgnDetectionLevel
+
 						smallPip = False
 					else:
 						smallPip = True
 				timeZeroDetected = time[i]
 				diffTimeZeroDetectedOld = diffTimeZeroDetected
 
-		if cpsType == 'single_pip_tci' or cpsType == 'single_pip_cdi' or ((cpsType == 'dual_pip_tci' or cpsType == 'dual_pip_cdi') and cpsZero > 2 and smallPip == False):
+		if cpsType == 'single_pip_tci' or cpsType == 'single_pip_cdi' or \
+		((cpsType == 'dual_pip_tci' or cpsType == 'dual_pip_cdi') and cpsZero > 2 and smallPip == False):
 			if(minZeroLevel > cps[i]):
 				minZeroLevel = cps[i]
 				timeMinZeroLevel = time[i]			
@@ -191,7 +222,7 @@ for i in range(len(cps)):
 			ignAngleList.append(round(ignAngle[i],2))
 			minZeroLevel = 0
 			if itSparked == False and (cpsType == 'single_pip_tci' or cpsType == 'dual_pip_tci'):
-				print 'ign miss at ',time[i]
+				print 'ign miss at ',time[i],'ms'
 			itSparked = False
 		cpsZeroLevelDetected = False
 
@@ -248,20 +279,40 @@ if 0:
 		pass
 
 
-avgRpm = round(np.array(rpmFromZeroList).mean(),2)
-avgIgnAngle = round(np.array(ignAngleList[1:len(ignAngleList)-1]).mean(),2)
+dwellOnTimeStdDev = 0
+
+
+avgRpm = round(np.array(rpmFromZeroList[1:len(rpmFromZeroList)-1]).mean(),2)
+rpmStdDev = round(np.array(rpmFromZeroList[1:len(rpmFromZeroList)-1]).std(),2)	
+
+avgIgnAngle = round(np.array(ignAngleList[3:len(ignAngleList)-1]).mean(),2)
+ignAngleStdDev = round(np.array(ignAngleList[3:len(ignAngleList)-1]).std(),2)	
+
+
+# print '\nRpm:\n',rpmFromZeroList[1:len(rpmFromZeroList)-1]		
+# print '\nIgn angles in degree (before zero pulse min point):\n',ignAngleList[1:len(ignAngleList)-1]
+
+# 1 space 4 tabs, 1 space 8 tabs
+print '\n\nEntity 				Average 					Std dev'
+print '\nRpm 				', avgRpm,'						',rpmStdDev 
+print 'Ign Angle (degrees)		', avgIgnAngle,'						',ignAngleStdDev, 
 
 if cpsType == 'single_pip_tci' or cpsType == 'dual_pip_tci':
-	avgDwellOnTime = round(np.array(timeDwellOnList[1:len(timeDwellOnList)-1]).mean(),2)
-print '\nSuccessfully completed mapping.'
-print '\nRpm:\n',rpmFromZeroList		
-print '\nIgn angles in degree (before zero pulse):\n',ignAngleList[1:len(ignAngleList)-1]
-if cpsType == 'single_pip_tci' or cpsType == 'dual_pip_tci':
-	print '\nDwell on times (ms):\n',timeDwellOnList[1:len(timeDwellOnList)-1]
-print '\n\nAverage rpm = ', avgRpm	
-print '\nAverage ign angle = ',avgIgnAngle,'degrees'
-if cpsType == 'single_pip_tci' or cpsType == 'dual_pip_tci':
-	print '\nAverage dwell on time = ', avgDwellOnTime,'ms'
+	avgDwellOnTime = round(np.array(timeDwellOnList[1:len(timeDwellOnList)-1]).mean(),2)	
+	dwellOnTimeStdDev = round(np.array(timeDwellOnList[1:len(timeDwellOnList)-1]).std(),2)
+	# print '\nAverage dwell on time = ', avgDwellOnTime,'ms', '		Std dev in dwell on time = ',dwellOnTimeStdDev,'ms'	
+	print '\nDwell on time (ms)		', avgDwellOnTime,'						',dwellOnTimeStdDev, 
+
+mappingFailed = False
+if rpmStdDev > 150 or math.isnan(rpmStdDev):
+	print '\n\nSomething went wrong with the rpm. Change the zero detection level if necessary.'
+	mappingFailed = True
+if ignAngleStdDev > 5 or math.isnan(ignAngleStdDev):
+	print '\n\nSomething went wrong with the ign angle. Change the ign detection level if necessary.'
+	mappingFailed = True
+
+if mappingFailed == False:
+	print '\n\nInput data looks good. Successfully completed mapping.'
 
 
-
+# print '\n\n\nentered = ',entered,	' cpsZero = ', cpsZero
